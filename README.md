8b5a4a8c4e6303dc66d290cee593fe587dcf62bd
Marin Tsanov <btg.tsanov.marin@gmail.com>
2020-06-26 18:53:07 +0300

    Initial commit for the database structure file shootthis.sql

--- README.md
+++ README.md
@@ -9,6 +9,9 @@ A 2D HTML5 shooter, using the [Phaser 3](https://github.com/photonstorm/phaser/)
 3. As soon as you put this project onto your web server it should work perfectly fine for the most part. You may have to configure some parameters (i.e. database details and recaptcha keys) in the file register.php.
 4. The mail verfication script may not work when it's requested from a location that's behind proxy/CDN and thus break down the register.php file. You may want to configure your web server to serve that script directly. **When you do so, you need to configure $mailValidatorHost, by default it is $_SERVER['HTTP_HOST']. The mail validator script could have some unexpected behavior when requested using https protocol, so http may only be used. Since that's an inside page, it's perfectly fine.**
 
+## About the SQL file included
+This is the structure of the database that is used for ShootThis **and it is required by both the frontend and the backend.**
+
 ## Authors
 - Web Design: M. Tsanov
 - Game Design: S. Tsvetkov
---
+++ shootthis.sql
@@ -0,0 +1,115 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 26, 2020 at 05:50 PM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.1.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `shootthis`
+--
+CREATE DATABASE IF NOT EXISTS `shootthis` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `shootthis`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `loginhistory`
+--
+
+CREATE TABLE `loginhistory` (
+  `id` int(11) NOT NULL,
+  `username` varchar(16) NOT NULL,
+  `time` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `servers`
+--
+
+CREATE TABLE `servers` (
+  `id` int(11) NOT NULL,
+  `serverName` varchar(255) NOT NULL,
+  `serverIP` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(16) NOT NULL,
+  `nickname` varchar(16) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `regdate` datetime NOT NULL DEFAULT current_timestamp(),
+  `email` varchar(255) NOT NULL,
+  `ipfromreg` varchar(45) NOT NULL,
+  `ip` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `loginhistory`
+--
+ALTER TABLE `loginhistory`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `servers`
+--
+ALTER TABLE `servers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `loginhistory`
+--
+ALTER TABLE `loginhistory`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `servers`
+--
+ALTER TABLE `servers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
